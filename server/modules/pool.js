3e1d787d601fc57604417f658a2a7f38c686a24e
Lee Xiong <tadbitz@yahoo.com>
2023-03-13 20:06:53 -0500

    update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,10 +1,23 @@
 const pg = require('pg');
+let pool;
 
-// DB CONNECTION
-const pool = new pg.Pool({
-    database: 'SQL_To_do',
-    host: 'localhost',
-    port: 5432
-});
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: flase
+        }
+    });
+}
+else { 
+    pool = new pg.Pool({
+        database: 'SQL_To_do',
+        host: 'localhost',
+        port: 5432
+    });
+}
+        
+  
+
 
 module.exports = pool;
